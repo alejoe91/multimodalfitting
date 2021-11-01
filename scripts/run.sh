dcd541a6a94d99e33351fc078709964c481c4bee
Tanguy Damart <tanguy.damart@epfl.ch>
2021-11-01 09:39:14 +0100

    Minor bug fix

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,12 +1,10 @@
 export OPT_MODEL='hay'  # hay | hay_ais | experimental
-export OPT_EXTRA_STRATEGY='all'  # all | single | sections
 
 # Define model and experimental folders here. They should contain:
 # features_BPO.json, protocols_BPO.json, probe_BPO.json,
-exp_folder='../data/experimental/cell1_210301/efeatures'
-hay_folder='../data/hay_ecode_probe_planar/efeatures'
-hayais_folder='../data/hay_ais_ecode_probe_planar/efeatures'
-hayaishillock_folder='../data/hay_ais_hillock_ecode_probe_planar/efeatures'
+exp_folder='../data/experimental/210301_3113_cell1/efeatures'
+hay_folder='../data/models/hay_ecode_probe_planar/efeatures'
+hayais_folder='../data/models/hay_ais_ecode_probe_planar/efeatures'
 
 export CELL_FOLDER="../cell_models"
 export OPT_FOLDER="../optimization_results"
@@ -21,13 +19,18 @@ then
 elif [ $OPT_MODEL == "hay_ais" ];
 then
   export DATA_FOLDER=$hayais_folder
-elif [ $OPT_MODEL == "hay_ais_hillock" ];
-then
-  export DATA_FOLDER=$hayaishillock_folder
 fi
 
-for feature_set in 'soma' 'extra'; do
-      export OPT_FEATURE_SET=${feature_set}
+export OPT_FEATURE_SET='soma'
+export OPT_EXTRA_STRATEGY='all'
+for seed in {1..10}; do
+  export OPT_SEED=${seed}
+  sbatch ipyparallel.sbatch
+done
+
+export OPT_FEATURE_SET='extra'
+for strategy in 'all' 'single' 'sections'; do
+      export OPT_EXTRA_STRATEGY=${strategy}
       for seed in {1..10}; do
           export OPT_SEED=${seed}
           sbatch ipyparallel.sbatch
